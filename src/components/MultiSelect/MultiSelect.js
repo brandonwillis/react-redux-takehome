16b7ca70f694676e378bce75bbdb2644cb3053da
Brandon Willis <brandonlynnwillis@gmail.com>
2021-03-31 22:29:16 -0700

    adds multiselect change

--- src/components/MultiSelect/MultiSelect.js
+++ src/components/MultiSelect/MultiSelect.js
@@ -13,16 +13,6 @@ function MultiSelect({ title, options, onChangeHandler, selected, errorMessage, 
                         </div>
                     ))
                 }
-                {/* {
-                    Object.entries(options).sort((x, y) => x[1].name.localeCompare(y[1].name)).map(([key, value]) => {
-                        return (
-                            <div key={value.name} className={styles.row}>
-                                <input type="checkbox" value={key} checked={selected[key] || false} onChange={onChangeHandler(value)} />
-                                <label onClick={onChangeHandler(value)}>{value.name}</label>
-                            </div>
-                        )
-                    })
-                } */}
             </div>
             <div className={styles.errorTextContainer}>{errorMessage}</div>
         </div>
